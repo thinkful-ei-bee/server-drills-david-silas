2db6899c028e43e05fa20c23070fb7117ce2800c
Silas <silasishallahan@gmail.com>
2019-04-01 09:15:25 -0700

    completed cipher

--- index.js
+++ index.js
@@ -14,6 +14,26 @@ app.get('/sum', (req, res) => {
   res.send(`The sum of ${queryA} and ${queryB} is ${sum}`);
 });
 
+app.get('/cipher', (req, res) => {
+  const query = req.query;
+
+  const text = query.text;
+  const shift = Number(query.shift);
+
+  let encrypted = '';
+
+  for(let i = 0; i < text.length; i++) {
+    const letter = text[i].toUpperCase().charCodeAt(0);
+    if(letter < 65 || letter > 90) {
+      encrypted += String.fromCharCode(letter)
+    } else {
+      encrypted += String.fromCharCode(((letter + shift - 65) % 26) + 65);
+    }
+  }
+
+  res.send(encrypted);
+});
+
 app.listen(8000, () => {
   console.log('listening on 8000');
 });
